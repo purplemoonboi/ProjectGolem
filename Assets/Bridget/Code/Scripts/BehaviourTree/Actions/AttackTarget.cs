bee2d871c3bc8b9c7a7bf88750e7e1d8a8e60db6
BridgetACasey <bridgetannabelcasey@yahoo.com>
2022-02-28 20:41:18 +0000

    Resolving conflicts

--- Assets/Bridget/Code/Scripts/BehaviourTree/Actions/AttackTarget.cs
+++ Assets/Bridget/Code/Scripts/BehaviourTree/Actions/AttackTarget.cs
@@ -17,26 +17,26 @@ public class AttackTarget : ActionNode
 
     protected override State OnUpdate()
     {
-        if (blackboard.target == null)
-            return State.Failure;
-
-        Vector3 direction = blackboard.target.transform.position - context.transform.position;
-        distanceToTarget = direction.magnitude;
-
-        //Debug.Log("Distance to Target: " + distanceToTarget);
-
-        if(distanceToTarget < 10.0f)
-        {
-            blackboard.target.SetHealth(blackboard.target.GetHealth() - 1.0f);
-        }
-        else
-        {
-            return State.Failure;
-        }
-
-        if (blackboard.target.GetHealth() <= 0.0f)
-            return State.Success;
-
-        return State.Running;
+     // if (blackboard.target == null)
+     //     return State.Failure;
+     //
+     // Vector3 direction = blackboard.target.transform.position - context.transform.position;
+     // distanceToTarget = direction.magnitude;
+     //
+     // //Debug.Log("Distance to Target: " + distanceToTarget);
+     //
+     // if(distanceToTarget < 10.0f)
+     // {
+     //     blackboard.target.SetHealth(blackboard.target.GetHealth() - 1.0f);
+     // }
+     // else
+     // {
+     //     return State.Failure;
+     // }
+     //
+     // if (blackboard.target.GetHealth() <= 0.0f)
+     //     return State.Success;
+     //
+      return State.Running;
     }
 }
